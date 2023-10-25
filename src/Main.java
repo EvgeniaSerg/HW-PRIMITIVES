9c70c470fb76a4713719da723e317f6d56a91b98
Eugenia <st_shelkunova@list.ru>
2023-10-25 20:37:34 +0300

    src

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 5653;
-        int condition = 20;
-        int mili = amount / condition;
+        int amount = 5653; // стоимость билета
+        int condition = 20; // количество рублей для одной бонусной мили
+        int mili = amount / condition; // 
 
         System.out.println("Начислится миль: " + mili);
 
