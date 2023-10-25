aeb32bad418cbba2d6a220342073d32922bc06fc
Eugenia <st_shelkunova@list.ru>
2023-10-25 20:26:55 +0300

    src

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
 
+        int amount = 5653;
+        int condition = 20;
+        int mili = amount / condition;
+
+        System.out.println("Начислится миль: " + mili);
+
+    }
+}
